e67cc1fc886969077c99d749dad1a304ab5a579e
Josh Cook <jcook2303@gmail.com>
2023-03-22 20:24:38 -0700

    First commit for assignment. I am honestly confused on how this is implemented. I understand the general idea of the assignment but am needing to check solutions to help me.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,17 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    i = 0
+    if block_given?
+      for elem in self do
+        yield elem, i
+        i += 1
+      end
+      return self
+    else
+      puts "No block given"
+    end
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +19,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
+  include Enumerable
   # Define my_each here
+  def my_each
+    for elem in self do
+      yield elem
+    end
+    return self
+  end
 end
